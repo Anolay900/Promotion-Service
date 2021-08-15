a915011d7615da754cea00b0545a57af5df9c760
Anolay900 <andalrom@gmail.com>
2021-08-16 01:52:31 +0300

    theme

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,9 @@ themes.onclick = () => {
     document.body.classList.toggle("dark")
     localStorage.theme = document.body.className || "light"
 }
+
+$(document).ready(function() {
+    $( ".arrow" ).click( function() {
+        $("#image").toggleClass('flip');
+    });
+});
